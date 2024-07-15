4483b616b54b44440c14ae885c2b7cbd16258bad
felipe.silva <felipe-dev@outlook.com>
2024-07-15 14:46:33 -0300

    Validacao para não duplicar login

--- src/main/kotlin/com/example/thinkot_2/repositories/CredentialRepository.kt
+++ src/main/kotlin/com/example/thinkot_2/repositories/CredentialRepository.kt
@@ -1,6 +1,11 @@
 package com.example.thinkot_2.repositories
 
 import com.example.thinkot_2.entities.Credential
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 
-interface CredentialRepository : CrudRepository<Credential, Int>
+interface CredentialRepository : CrudRepository<Credential, Int>{
+     @Query("SELECT CASE WHEN COUNT(c) > 0 THEN TRUE ELSE FALSE END from Credential c where c.login = :login")
+     fun existsByLogin(login:String?) : Boolean
+
+}
--- src/main/kotlin/com/example/thinkot_2/services/UserServices.kt
+++ src/main/kotlin/com/example/thinkot_2/services/UserServices.kt
@@ -8,11 +8,14 @@ import com.example.thinkot_2.services.interfaces.PhoneServicesInterface
 import com.example.thinkot_2.services.interfaces.UserServicesInterface
 import jakarta.transaction.Transactional
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 
 @Service
-class UserServices(@Autowired private val userRepository: UserRepository, @Autowired private val phoneNumberServices: PhoneNumberServices):UserServicesInterface,
+class UserServices(
+    @Autowired private val userRepository: UserRepository,
+    @Autowired private val phoneNumberServices: PhoneNumberServices,
+    @Autowired private val credentialService: CredentialService
+) : UserServicesInterface,
     PhoneServicesInterface {
 
     fun validateUserForm(userForm: UserForm): UserFormResult {
@@ -20,27 +23,38 @@ class UserServices(@Autowired private val userRepository: UserRepository, @Autow
 
         val retUser: Map<String, String> = validateUser(userForm.user);
 
-        retUser.forEach() {(campo, erro) ->
+        retUser.forEach() { (campo, erro) ->
             userFormResult.addUserError(campo, erro)
         }
 
+        var skipPhoneValidation = false;
         userForm.phone.forEach { phoneNumber ->
-            if (phoneNumberServices.phoneNumberExists(phoneNumber)){
-                userFormResult.addUserError("duplicate_number", "Combinação de DDI, DDD e Numero já existem na base de dados")
+            if (phoneNumberServices.phoneNumberExists(phoneNumber)) {
+                userFormResult.addUserError(
+                    "duplicate_number",
+                    "Combinação de DDI (${phoneNumber.phoneDDI}), DDD (${phoneNumber.phoneDDD}) e Numero ${phoneNumber.phoneNumber} já existem na base de dados"
+                )
+                skipPhoneValidation = true;
             }
         }
 
-        val retPhones: Map<String, String>  = validatePhones(userForm.phone);
-
-        retPhones.forEach() {(campo, erro) ->
-            userFormResult.addUserError(campo, erro)
+        if (!skipPhoneValidation) {
+            val retPhones: Map<String, String> = validatePhones(userForm.phone);
+            retPhones.forEach() { (campo, erro) ->
+                userFormResult.addUserError(campo, erro)
+            }
         }
 
-        val retCredential: Map<String, String>  = validateCredential(userForm.credential);
+        if (!credentialService.loginExists(userForm.credential)) {
 
-        retCredential.forEach() {(campo, erro) ->
-            userFormResult.addUserError(campo, erro)
+            val retCredential: Map<String, String> = validateCredential(userForm.credential);
+            retCredential.forEach() { (campo, erro) ->
+                userFormResult.addUserError(campo, erro)
+            }
+        } else {
+            userFormResult.addUserError("duplicate_login", "O login ${userForm.credential.login} já está em uso")
         }
+
 
         return userFormResult;
     }
